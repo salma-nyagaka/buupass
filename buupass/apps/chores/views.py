abf49a5c89121ef3df394bb2766f96807f19e4b9
salma-nyagaka <salmanyagaka@gmail.com>
2021-02-13 23:36:09 +0300

    Added response messages

--- buupass/apps/chores/views.py
+++ buupass/apps/chores/views.py
@@ -1,9 +1,12 @@
-from .models import Chores
-from .serializers import ChoresSerializer
 from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+
+from buupass.helpers.endpoint_response import \
+    get_success_responses
+from .models import Chores
+from .serializers import ChoresSerializer
 
 
 class ChoresList(APIView):
@@ -13,17 +16,29 @@ class ChoresList(APIView):
     def get(self, request, format=None):
         chores = Chores.objects.all()
         serializer = ChoresSerializer(chores, many=True)
-        return Response(serializer.data)
+        return get_success_responses(
+            data=serializer.data,
+            message="Successfully fetched all the chores",
+            status_code=status.HTTP_201_CREATED
+        )
 
     def post(self, request, format=None):
         serializer = ChoresSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+            return get_success_responses(
+                data=serializer.data,
+                message="Successfully created a chore",
+                status_code=status.HTTP_201_CREATED
+            )
+        return get_success_responses(
+                data=serializer.errors,
+                message="Error Message",
+                status_code=status.HTTP_400_BAD_REQUEST
+            )
 
 class ChoresDetail(APIView):
+    """Get a specific chore"""
     def get_object(self, pk):
         try:
             return Chores.objects.get(pk=pk)
@@ -35,10 +50,28 @@ class ChoresDetail(APIView):
         serializer = ChoresSerializer(chore, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return get_success_responses(
+                data=serializer.data,
+                message="Successfully created a chore",
+                status_code=status.HTTP_200_OK
+            )
+        return get_success_responses(
+                data=serializer.errors,
+                message="Error Message",
+                status_code=status.HTTP_400_BAD_REQUEST
+            )
 
     def get(self, request, pk, format=None):
         chore = self.get_object(pk)
         serializer = ChoresSerializer(chore)
-        return Response(serializer.data)
+        return get_success_responses(
+            data=serializer.data,
+            message="Successfully fetched the chore",
+            status_code=status.HTTP_200_OK
+        )
+   
+
+    def delete(self, request, pk, format=None):
+        chore = self.get_object(pk)
+        chore.delete()
+        return Response({"Success: uccessfully deleted the chore"}, status=status.HTTP_200_OK)
