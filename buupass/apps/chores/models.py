c10f4082c7dfe60685963f011f4b8f1a4ad8ec9f
salma-nyagaka <salmanyagaka@gmail.com>
2021-02-13 22:15:50 +0300

    Added chores model

---
+++ buupass/apps/chores/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from datetime import datetime
+from buupass import settings
+from buupass.helpers.fancy_generator import fancy_id_generator
+
+
+class Chores(models.Model):
+    id = models.CharField(db_index=True,
+                        max_length=256,
+                        default=fancy_id_generator,
+                        primary_key=True,
+                        editable=False)
+    title = models.CharField(max_length=30)
+    summary = models.TextField(max_length=300)
+    completed = models.BooleanField(default=False)
+    date_completed = models.DateTimeField('date completed')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+def save(self, *args, **kwargs):
+    if self.completed == True:
+        self.date_completed = datetime.now()
+    super(Chores, self).save(*args, **kwargs)
--- buupass/settings.py
+++ buupass/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'buupass.apps.authentication',
-
+    'buupass.apps.chores'
 ]
 
 MIDDLEWARE = [
